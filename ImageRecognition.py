e8b8c639a3c143876359bd94ca0a3765b80cf3d8
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2021-07-12 23:19:52 +0530

    Update: 1. Added a function to check valid file extension given the name of the file. 2. Added bare necessary code to start flask app.

--- ImageRecognition.py
+++ ImageRecognition.py
@@ -1,4 +1,4 @@
-import flask import Flask, request, url_for, render_template
+from flask import Flask, request, url_for, render_template
 import os
 from werkzeug import secure_filename
 from tensorflow.keras.applications import MobileNet
@@ -14,3 +14,17 @@ ALLOWED_EXTENSION = set(['txt', 'pdf', 'png','jpg','jpeg','gif'])
 IMAGE_HEIGHT = 224
 IMAGE_WIDTH = 224
 IMAGE_CHANNELS = 3
+
+# Function to check if extension of a file is allowed
+def allowed_file(filename):
+    # Split with '.' and maximum splits is 1
+    return filename.rsplit('.', 1)[1] in ALLOWED_EXTENSION
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello'
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=False)
